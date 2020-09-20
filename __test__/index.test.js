4d26805f75b8113c31bb25c6db2fd436c96adab9
INAI Yosuke <yosuke_inai@ohq.jp>
2020-09-20 18:57:45 +0900

    5th commit

--- __test__/index.test.js
+++ __test__/index.test.js
@@ -1,23 +1,23 @@
 
 
-
+const expect = require('expect');
 const fizzbuzz = require ("../index");
 
 describe("fizzbuzz", ()=> {
   it ("returns FizzBuzz when value is divisible by 15", () => {
-    expect(fizzbuzz(30)).toStrictEqual("FizzBuzz");
+    expect(fizzbuzz(30)).toBe("FizzBuzz");
   });
 
   it ("returns FizzBuzz when value is divisible by 3", () => {
-    expect(fizzbuzz(9)).toStrictEqual("Fizz");
+    expect(fizzbuzz(9)).toBe("Fizz");
   });
 
   it ("returns FizzBuzz when value is divisible by 5", () => {
-    expect(fizzbuzz(10)).toStrictEqual("Buzz");
+    expect(fizzbuzz(10)).toBe("Buzz");
   });
 
   it ("returns FizzBuzz when value is divisible by 3 or 5", () => {
-    expect(fizzbuzz(7)).toStrictEqual("7");
+    expect(fizzbuzz(7)).toBe("7");
   });
 });
 
