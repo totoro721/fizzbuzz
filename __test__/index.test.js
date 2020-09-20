db4240f011fe7155820bbf5a65a4f5437d22d76d
INAI Yosuke <yosuke_inai@ohq.jp>
2020-09-20 17:28:02 +0900

    2nd commit

--- __test__/index.test.js
+++ __test__/index.test.js
@@ -3,21 +3,21 @@
 
 const fizzbuzz = require ("../index");
 
-descrie("fizzbuzz", ()=> {
+describe("fizzbuzz", ()=> {
   if ("returns FizzBuzz when value is divisible by 15", () => {
-    expect(fizzbuzz(30).toStrictEqual("FizzBuzz");
+    expect(fizzbuzz(30)).toStrictEqual("FizzBuzz");
   });
 
   if ("returns FizzBuzz when value is divisible by 3", () => {
-    expect(fizzbuzz(9).toStrictEqual("Fizz");
+    expect(fizzbuzz(9)).toStrictEqual("Fizz");
   });
 
   if ("returns FizzBuzz when value is divisible by 5", () => {
-    expect(fizzbuzz(10).toStrictEqual("Buzz");
+    expect(fizzbuzz(10)).toStrictEqual("Buzz");
   });
 
   if ("returns FizzBuzz when value is divisible by 3 or 5", () => {
-    expect(fizzbuzz(7).toStrictEqual("7");
+    expect(fizzbuzz(7)).toStrictEqual("7");
   });
 });
 
